9841d1fd01ac695d3ddee319a63630d99c658216
Jinli Liang <jinli@datarepublic.com.au>
2017-12-18 15:20:21 +1100

    fix UnitTest

--- hcatalog/webhcat/svr/src/test/java/org/apache/hive/hcatalog/templeton/HcatDelegatorTest.java
+++ hcatalog/webhcat/svr/src/test/java/org/apache/hive/hcatalog/templeton/HcatDelegatorTest.java
@@ -1,8 +1,17 @@
 package org.apache.hive.hcatalog.templeton;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 
 public class HcatDelegatorTest extends TestCase {
+
+    @Before
+    public void setUp(){
+        final String[] args = new String[] {};
+        final Main main = new Main(args);
+        final AppConfig config = main.getAppConfigInstance();
+    }
+
     public void testGetCatalogStatementWithUse() {
         HcatDelegator.SchemaStatement cs = HcatDelegator.getSchemaStatement("use db1; show create table x;");
         assertEquals("db1", cs.schema);
